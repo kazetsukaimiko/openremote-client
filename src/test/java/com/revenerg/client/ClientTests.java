f65d5c4d014d67fd5c6a8da94db199f7a408ad0f
Kazetsukai <kazetsukai@protonmail.com>
2023-09-04 16:06:46 -0400

    ISG Root Cert connect example

--- src/test/java/com/revenerg/client/ClientTests.java
+++ src/test/java/com/revenerg/client/ClientTests.java
@@ -1,41 +1,46 @@
 package com.revenerg.client;
 
 import com.revenerg.util.ssl.AllTrustingSocketFactory;
+import com.revenerg.util.ssl.CACertSocketFactory;
 import lombok.extern.jbosslog.JBossLog;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.junit.jupiter.api.Test;
-
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.junit.jupiter.api.Test;
 
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
 import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.TrustManagerFactory;
-import javax.net.ssl.X509TrustManager;
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.KeyManagementException;
-import java.security.KeyStore;
+import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
-import java.security.Security;
 import java.security.cert.CertificateException;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
+import java.util.Map;
+import java.util.Properties;
 
 @JBossLog
 public class ClientTests {
 
+    Path PROPERTIES_FILE = Paths.get(System.getProperty("user.home"), "openremote.properties");
+    Path CA_FILE = Paths.get(System.getProperty("user.home"), "sheesh", "openremote", "certs", "isrgrootx1.pem");
 
     @Test
-    public void testConnectingToOpenRemote() throws MqttException, NoSuchAlgorithmException, KeyManagementException {
+    public void testConnectingToOpenRemote() throws MqttException, NoSuchAlgorithmException, KeyManagementException, IOException, InterruptedException, CertificateException, KeyStoreException {
+        Properties p = new Properties();
+        p.load(Files.newInputStream(PROPERTIES_FILE));
 
-        String clientId = "clientId";
-        String address = "ssl://104.37.189.62:8883";
+        String address = p.getProperty("address", null);
+        String clientId = p.getProperty("clientId", null);
+        String realm =  p.getProperty("realm", null);
+        String username =  p.getProperty("username", null);
+        String secret =  p.getProperty("secret", null);
+
+        String mqttUser = realm + ":" + username;
 
         log.infof("Creating MqttClient for %s as %s", address, clientId);
 
@@ -43,9 +48,15 @@ public class ClientTests {
         try (MqttClient mqttClient = new MqttClient(address, clientId, persistence)) {
             MqttConnectOptions options = new MqttConnectOptions();
 
+            log.infof("Setting username '%s'.", mqttUser);
+            options.setUserName(mqttUser);
+            options.setPassword(secret.toCharArray());
+
             // SSL.
-            SSLSocketFactory socketFactory = AllTrustingSocketFactory.INSTANCE.getFactory();
-            options.setHttpsHostnameVerificationEnabled(false); // TODO Fix this with a CA.
+            SSLSocketFactory socketFactory =
+                    //AllTrustingSocketFactory.INSTANCE.getFactory();
+                    new CACertSocketFactory(CA_FILE).getFactory();
+            
             options.setSocketFactory(socketFactory);
 
             options.setCleanSession(true);
@@ -60,17 +71,20 @@ public class ClientTests {
 
             var responseTopic = OpenRemoteClient.PROVISIONING_RESPONSE_TOPIC.formatted(clientId);
             log.infof("Subscribing to '%s'".formatted(responseTopic));
-            mqttClient.subscribe(responseTopic);
+            mqttClient.subscribe(responseTopic, this::receiveResponse);
 
             Thread.sleep(5000);
 
             log.infof("Disconnecting.");
             mqttClient.disconnect();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
         }
 
 
+    }
+
+    private void receiveResponse(String topic, MqttMessage mqttMessage) {
+        log.infof("Received message on topic '%s':\n%s",
+                topic, new String(mqttMessage.getPayload(), StandardCharsets.UTF_8));
     }
 
 }
